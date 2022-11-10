af622d38b32877a8c005525c8ac12976069f83ae
vijay dhyani <vijay.dhyani@ballisticlearning.com>
2022-11-10 11:42:51 +0530

    payment page added

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
-const continueNameBtn = document.querySelector('.continue-one')
-const userName = document.querySelector('.username')
-const screenOne = document.querySelector('.screen1')
-const screenTwo = document.querySelector('.screen2')
-const backScreenOne = document.querySelector('.back-s1')
-const backScreenTwo = document.querySelector('.back-s2')
-const cardText = document.querySelector('.dynamic-card-text')
-const cardNumber = document.querySelector('.cardnumber')
+const continueNameBtn = document.querySelector(".continue-one");
+const continueCardBtn = document.querySelector(".continue-two");
+const userName = document.querySelector(".username");
+const screenOne = document.querySelector(".screen1");
+const screenTwo = document.querySelector(".screen2");
+const screenThree = document.querySelector(".screen3");
+const backScreenOne = document.querySelector(".back-s1");
+const backScreenTwo = document.querySelector(".back-s2");
+const backScreenThree = document.querySelector(".back-s3");
+const cardText = document.querySelector(".dynamic-card-text");
+const cardNumber = document.querySelector(".cardnumber");
 
+continueNameBtn.addEventListener("click", function () {
+  console.log(userName.value);
+  screenOne.classList.add("d-none");
+  screenTwo.classList.remove("d-none");
+});
+continueCardBtn.addEventListener("click", function () {
+  screenOne.classList.add("d-none");
+  screenTwo.classList.add("d-none");
+  screenThree.classList.remove("d-none");
+});
 
+backScreenTwo.addEventListener("click", function () {
+  screenOne.classList.remove("d-none");
+  screenTwo.classList.add("d-none");
+});
 
-continueNameBtn.addEventListener('click', function () {
-    console.log(userName.value);
-    screenOne.classList.add('d-none')
-    screenTwo.classList.remove('d-none')
-})
+backScreenThree.addEventListener("click", function () {
+  screenOne.classList.add("d-none");
+  screenTwo.classList.remove("d-none");
+  screenThree.classList.add("d-none");
+});
 
-backScreenTwo.addEventListener('click', function () {
-    screenOne.classList.remove('d-none')
-    screenTwo.classList.add('d-none')
-})
-
-cardNumber.addEventListener('keyup', function () {
-    console.log(cardNumber.value);
-    cardText.textContent = `${cardNumber.value}`
-})
+cardNumber.addEventListener("keyup", function () {
+  console.log(cardNumber.value);
+  cardText.textContent = `${cardNumber.value}`;
+});
